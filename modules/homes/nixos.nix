cc495c256492280da18b21d62f81706880e1e2f0
Pierre Godard <pierre-godard@users.noreply.github.com>
2026-08-23 22:44:24 +0000

    chore: remove obsolete files and standalone home configs
    - Delete modules/hosts/wsl/default.nix (was empty placeholder)
    - Delete modules/features/home-manager.nix (home-manager config now lives in each host aspect)
    - Strip old flake.homeConfigurations.* and flake.homeModules.* from homes/pierre.nix and homes/nixos.nix
      (den now drives home-manager via the home-manager battery)

--- modules/homes/nixos.nix
+++ modules/homes/nixos.nix
@@ -1,28 +1,4 @@
-{ self, inputs, den, ... }: {
-  flake.homeConfigurations.nixosConfiguration = inputs.home-manager.lib.homeManagerConfiguration {
-    pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
-    modules = [
-      self.homeModules.nixosModule
-      {
-        home.username = "nixos";
-        home.homeDirectory = "/home/nixos";
-      }
-    ];
-  };
-
-  flake.homeModules.nixosModule = { pkgs, ... }: {
-    imports = [
-      self.homeModules.catppuccin
-      self.homeModules.ghostty
-      self.homeModules.firefox
-      self.homeModules.shell
-      self.homeModules.development
-      self.homeModules.github
-    ];
-    home.username = "nixos";
-    home.stateVersion = "26.05";
-  };
-
+{ den, ... }: {
   den.aspects.nixos = {
     includes = [
       den.aspects.catppuccin
--- modules/homes/pierre.nix
+++ modules/homes/pierre.nix
@@ -1,28 +1,4 @@
-{ self, inputs, den, ... }: {
-  flake.homeConfigurations.pierreConfiguration = inputs.home-manager.lib.homeManagerConfiguration {
-    pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
-    modules = [
-      self.homeModules.pierreModule
-      {
-        home.username = "pierre";
-        home.homeDirectory = "/home/pierre";
-      }
-    ];
-  };
-
-  flake.homeModules.pierreModule = { pkgs, ... }: {
-    imports = [
-      self.homeModules.catppuccin
-      self.homeModules.ghostty
-      self.homeModules.firefox
-      self.homeModules.shell
-      self.homeModules.development
-      self.homeModules.github
-    ];
-    home.username = "pierre";
-    home.stateVersion = "26.05";
-  };
-
+{ den, ... }: {
   den.aspects.pierre = {
     includes = [
       den.aspects.catppuccin
