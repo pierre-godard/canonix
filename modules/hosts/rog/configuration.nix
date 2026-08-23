79135a0853201a610039f9eee63d80bb974a8b33
Pierre Godard <pierre-godard@users.noreply.github.com>
2026-08-23 23:01:19 +0000

    feat: set timezone and locale on all hosts

--- modules/hosts/rog/configuration.nix
+++ modules/hosts/rog/configuration.nix
@@ -13,6 +13,8 @@
       nix.settings.experimental-features = [ "nix-command" "flakes" ];
       security.sudo.enable = true;
       system.stateVersion = "26.05";
+      time.timeZone = "Europe/Paris";
+      i18n.defaultLocale = "en_US.UTF-8";
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
       home-manager.backupFileExtension = ".backup";
--- modules/hosts/wsl/configuration.nix
+++ modules/hosts/wsl/configuration.nix
@@ -10,6 +10,8 @@
       wsl.defaultUser = "nixos";
       security.sudo.enable = true;
       system.stateVersion = "26.05";
+      time.timeZone = "Europe/Paris";
+      i18n.defaultLocale = "en_US.UTF-8";
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
       home-manager.backupFileExtension = ".backup";
