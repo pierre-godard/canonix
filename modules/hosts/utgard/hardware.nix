72945979a61810ebffa4e2a7ea5f89e817b7e03e
Pierre GODARD <3969168+pierre-godard@users.noreply.github.com>
2026-08-29 19:22:30 +0000

    feat(utgard): Change filesystem configuration to use UUIDs and btrfs
    Updated filesystem configurations to use UUIDs and changed root filesystem type to btrfs.

--- modules/hosts/utgard/hardware.nix
+++ modules/hosts/utgard/hardware.nix
@@ -7,17 +7,20 @@
       boot.initrd.kernelModules = [ ];
       boot.kernelModules = [ "kvm-intel" ];
       boot.extraModulePackages = [ ];
-      fileSystems."/" = {
-        device = "/dev/disk/by-label/nixos";
-        fsType = "ext4";
-      };
-      fileSystems."/boot" = {
-        device = "/dev/disk/by-label/boot";
-        fsType = "vfat";
-      };
-      swapDevices = [
-        { device = "/dev/disk/by-label/boot"; }
-      ];
+      fileSystems."/" =
+        { device = "/dev/disk/by-uuid/1e67285e-ed40-4469-9cd7-0e990aff2e04";
+          fsType = "btrfs";
+        };
+    
+      fileSystems."/boot" =
+        { device = "/dev/disk/by-uuid/D283-D8A0";
+          fsType = "vfat";
+          options = [ "fmask=0077" "dmask=0077" ];
+        };
+    
+      swapDevices =
+        [ { device = "/dev/disk/by-uuid/3ab84bca-f795-424e-a16b-80d96477e630"; }
+        ];
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
       hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     };
