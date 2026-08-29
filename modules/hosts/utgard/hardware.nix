54cc8e01020e36eac7d252a7a888682dc7bb384a
Pierre GODARD <3969168+pierre-godard@users.noreply.github.com>
2026-08-29 19:33:23 +0000

    fix(utgard): Fix availableKernelModules for initrd

--- modules/hosts/utgard/hardware.nix
+++ modules/hosts/utgard/hardware.nix
@@ -3,7 +3,7 @@
     nixos = { ... }: {
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
-      boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdm>
+      boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ]
       boot.initrd.kernelModules = [ ];
       boot.kernelModules = [ "kvm-intel" ];
       boot.extraModulePackages = [ ];
