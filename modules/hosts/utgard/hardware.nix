b5d8e21b884699ab74079879db301b9976890c68
Pierre GODARD <3969168+pierre-godard@users.noreply.github.com>
2026-08-29 19:21:19 +0000

    feat(utgard): Update hardware.nix with kernel modules and swap
    Added kernel modules and swap device configuration.

--- modules/hosts/utgard/hardware.nix
+++ modules/hosts/utgard/hardware.nix
@@ -3,6 +3,10 @@
     nixos = { ... }: {
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
+      boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdm>
+      boot.initrd.kernelModules = [ ];
+      boot.kernelModules = [ "kvm-intel" ];
+      boot.extraModulePackages = [ ];
       fileSystems."/" = {
         device = "/dev/disk/by-label/nixos";
         fsType = "ext4";
@@ -11,6 +15,11 @@
         device = "/dev/disk/by-label/boot";
         fsType = "vfat";
       };
+      swapDevices = [
+        { device = "/dev/disk/by-label/boot"; }
+      ];
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     };
   };
 }
