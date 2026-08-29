2ad6a19ac78dafee6d8637d28fa5ed5043df72b6
Pierre GODARD <3969168+pierre-godard@users.noreply.github.com>
2026-08-29 19:40:40 +0000

    fix(utgard): Refactor hardware.nix for improved structure

--- modules/hosts/utgard/hardware.nix
+++ modules/hosts/utgard/hardware.nix
@@ -1,26 +1,28 @@
-{ den, lib, ... }: {
+{ den, ... }: {
   den.aspects.utgard = {
-    nixos = { ... }: {
+    nixos = { config, lib, ... }: {
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
       boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
       boot.initrd.kernelModules = [ ];
       boot.kernelModules = [ "kvm-intel" ];
       boot.extraModulePackages = [ ];
-      fileSystems."/" =
-        { device = "/dev/disk/by-uuid/1e67285e-ed40-4469-9cd7-0e990aff2e04";
-          fsType = "btrfs";
-        };
-    
-      fileSystems."/boot" =
-        { device = "/dev/disk/by-uuid/D283-D8A0";
-          fsType = "vfat";
-          options = [ "fmask=0077" "dmask=0077" ];
-        };
-    
-      swapDevices =
-        [ { device = "/dev/disk/by-uuid/3ab84bca-f795-424e-a16b-80d96477e630"; }
-        ];
+
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/1e67285e-ed40-4469-9cd7-0e990aff2e04";
+        fsType = "btrfs";
+      };
+
+      fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/D283-D8A0";
+        fsType = "vfat";
+        options = [ "fmask=0077" "dmask=0077" ];
+      };
+
+      swapDevices = [
+        { device = "/dev/disk/by-uuid/3ab84bca-f795-424e-a16b-80d96477e630"; }
+      ];
+
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
       hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     };
