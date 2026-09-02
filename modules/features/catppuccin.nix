0612260af60397b2e458b057437a5d931597d906
Pierre Godard <pierre-godard@users.noreply.github.com>
2026-09-02 17:54:04 +0200

    feat(catppuccin): add GTK theming and cursor configuration

--- modules/features/catppuccin.nix
+++ modules/features/catppuccin.nix
@@ -4,9 +4,16 @@
       imports = [ inputs.catppuccin.nixosModules.catppuccin ];
       catppuccin = { flavor = "mocha"; enable = true; autoEnable = true; };
     };
-    homeManager = { ... }: {
+    homeManager = { pkgs, ... }: {
       imports = [ inputs.catppuccin.homeModules.catppuccin ];
       catppuccin = { flavor = "mocha"; enable = true; autoEnable = true; };
+      gtk.enable = true;
+      home.pointerCursor = {
+        package = pkgs.catppuccin-cursors.mochaDark;
+        name = "catppuccin-mocha-dark-cursors";
+        size = 24;
+        gtk.enable = true;
+      };
     };
   };
 }
